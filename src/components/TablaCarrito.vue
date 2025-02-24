27823fbdb0fda089b1fcca4946a61d541bb4a728
Anxo <anxopintosabreu25@gmail.com>
2025-02-24 11:32:21 +0100

    24/02/2025

--- src/components/TablaCarrito.vue
+++ src/components/TablaCarrito.vue
@@ -84,6 +84,13 @@ export default {
     },
 
     methods: {
+        calcularTotal() {
+            let total = 0
+            this.data.getCartItems.forEach(element => {
+                total += element.quantity * element.precio;
+            });
+            return total;
+        },
 
         async getArticulos() {
             try {
